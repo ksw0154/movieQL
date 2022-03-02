d0b012a43417296fb1700a29a57f735111c826ab
ksw0154 <rlatkddnjs44@naver.com>
2022-03-02 23:37:45 +0900

    schema, resolver 작성

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,40 @@
+const people = [
+  {
+    id: "1",
+    name: "Abel",
+    age: 20,
+    gender: "male",
+  },
+  {
+    id: "2",
+    name: "Abel",
+    age: 21,
+    gender: "male",
+  },
+  {
+    id: "3",
+    name: "Abel",
+    age: 22,
+    gender: "male",
+  },
+  {
+    id: "4",
+    name: "Abel",
+    age: 23,
+    gender: "male",
+  },
+];
+
 const resolvers = {
   Query: {
-    name: () => "Abel",
+    people: () => people,
+    person: (_, { id }) => getById(id),
   },
+};
+
+const getById = (id) => {
+  const filteredPeople = people.filter((person) => id === parseInt(person.id));
+  return filteredPeople[0];
 };
 
 export default resolvers;
